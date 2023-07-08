74812ad3a61b0424d7b274b81cc88fcc2a3cfc8c
igr-ok <lfsvrgd@gmail.com>
2023-07-08 12:48:50 +0100

    wip: Map component

---
+++ src/map/Map.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { MapContainer, TileLayer } from 'react-leaflet'
+
+
+const Map = () => {
+
+  return (
+    <>
+      <MapContainer 
+        center={[37.0902, -95.7129]} 
+        zoom={3} 
+        zoomControl={false} 
+        style={{ height: '100vh', width: '100%' }}
+      >
+        <TileLayer
+          attribution='&amp;copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </>
+  )
+}
+
+export default Map
